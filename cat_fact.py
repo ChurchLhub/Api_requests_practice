e189cd9c111618652d520df0b202f0d6514d4a0d
church <omorodjenoel9@gmail.com>
2026-07-06 02:09:01 +0100

    Created a random cat fact generator script

---
+++ cat_fact.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "https://catfact.ninja/fact"
+try:
+    response = requests.get(url,timeout=5)
+    response.raise_for_status()
+    data = response.json()
+    print(f"🐱 Random Cat Fact\n------------------\n{data["fact"]}")
+except requests.exceptions.ConnectionError:
+    print("Connection error occurred")
+
+except requests.exceptions.Timeout:
+    print("Request timed out")
+
+except requests.exceptions.HTTPError as http_err:
+    print(f"HTTP error: {http_err}")
+
+except ValueError:
+    print("Error parsing JSON response")    
