2be92433285da7733ae76af6d5ffe5da103d462e
church <omorodjenoel9@gmail.com>
2026-07-07 06:30:29 +0100

    Updated create_todo, to include a make post requests function with internal error handling

--- create_todo.py
+++ create_todo.py
@@ -1,5 +1,23 @@
 import requests
 
+def make_post_requests(item):
+    try:
+        response = requests.post(url,json=item,timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.ConnectionError:
+        print("Connection error occurred")
+
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error: {http_err}")
+
+    except ValueError:
+        print("Error parsing JSON response")   
+
+
 url = "https://jsonplaceholder.typicode.com/todos"
 
 todo = input ("Enter a new todo item: ").strip()
@@ -10,12 +28,6 @@ todo_item = {
     "title": todo,
 }
 
-response = requests.post(url, json=todo_item)
-code = response.status_code
-data = response.json()
-print(data)
-
-
-
-
+data =make_post_requests(todo_item)
+print(f"✅ New todo item created:\n{data}")
 
